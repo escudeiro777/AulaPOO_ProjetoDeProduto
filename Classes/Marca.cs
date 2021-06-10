c15f56a44803c6aa9fed287766a0b16665ab6584
Nathalia Novais <nathalianovais2004@gmail.com>
2021-06-10 13:08:10 -0300

    Marca finalizada

--- Classes/Marca.cs
+++ Classes/Marca.cs
@@ -1,7 +1,46 @@
+using System;
+using System.Collections.Generic;
+using AulaPOO_ProjetoProdutoSENAI.Interface;
+
 namespace AulaPOO_ProjetoProdutoSENAI.Classes
 {
-    public class Marca
+    public class Marca : IMarcacs
     {
-        
+        public int Codigo { get; set; }
+        public string NomeMarca { get; set; }
+        public DateTime DataCadastro { get; set; }
+        List<Marca> ListaMarca = new List<Marca>();
+
+        public Marca()
+        {
+
+        }
+        public Marca(int _codigo, string _nomemarca)
+        {
+            this.NomeMarca = _nomemarca;
+            this.Codigo = _codigo;
+            DataCadastro = DateTime.Now;
+        }
+
+
+        public string Cadastrar(Marca marca)
+        {
+            ListaMarca.Add(marca);
+            return "Marca cadastrada";
+        }
+
+        public string Deletar(Marca marca)
+        {
+            Console.WriteLine($"Qual o nome da marca que você deseja remover?");
+            string MarcaDeletada = Console.ReadLine();
+
+            ListaMarca.RemoveAll(x => x.NomeMarca == MarcaDeletada);
+            return "Deletado";
+        }
+
+        public List<Marca> Listar()
+        {
+            return ListaMarca;
+        }
     }
 }
--- Interface/IMarcacs.cs
+++ Interface/IMarcacs.cs
@@ -1,7 +1,15 @@
+using System.Collections.Generic;
+using AulaPOO_ProjetoProdutoSENAI.Classes;
+
 namespace AulaPOO_ProjetoProdutoSENAI.Interface
 {
     public interface IMarcacs
     {
-         
+        public interface IMarca
+        {
+            string Cadastrar(Marca marca);
+            string Deletar(Marca marca);
+            List<Marca> Listar();
+        }
     }
 }
